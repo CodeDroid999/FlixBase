c77dd2e3384636329bc23be940e23934702fbba3
CodeDroid99 <johnmwega2002@gmail.com>
2023-07-03 15:47:42 +0300

    add components and routing

--- flixbase/src/App.js
+++ flixbase/src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      Hello World!
-    </div>      
+      <Router>
+        <Routes>
+          <Route index element={<h1>Hello World!</h1>}></Route>
+          <Route path="movie/:id" element={<h1>Movie</h1>}></Route>
+          <Route path="movies/:type" element={<h1>Movielist</h1>}></Route>
+          <Route path="/*" element={<h1>Error Page</h1>}></Route>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
