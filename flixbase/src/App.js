f31cbcfc4e477df7676f2032b7a0ea206412092e
CodeDroid99 <johnmwega2002@gmail.com>
2023-07-03 18:17:44 +0300

    add header

--- flixbase/src/App.js
+++ flixbase/src/App.js
@@ -1,17 +1,19 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route index element={<h1>Hello World!</h1>}></Route>
-          <Route path="movie/:id" element={<h1>Movie</h1>}></Route>
-          <Route path="movies/:type" element={<h1>Movielist</h1>}></Route>
-          <Route path="/*" element={<h1>Error Page</h1>}></Route>
-        </Routes>
-      </Router>
+        <Router>
+          <Header />
+            <Routes>
+                <Route index element={<h1>Error Page</h1>}></Route>
+                <Route path="movie/:id" element={<h1>Error Page</h1>}></Route>
+                <Route path="movies/:type" element={<h1>Error Page</h1>}></Route>
+                <Route path="/*" element={<h1>Error Page</h1>}></Route>
+            </Routes>
+        </Router>
     </div>
   );
 }
--- flixbase/src/components/header/Header.js
+++ flixbase/src/components/header/Header.js
@@ -4,14 +4,15 @@ import { Link } from "react-router-dom"
 
 const Header = () => {
     return (
-        <div className="header" >
+        <div className="header">
             <div className="headerLeft">
-                <Link to="/"><img className="header__icon" src="/flixbase/public/flixabselogo.png" /></Link>
-                <Link to="/movies/popular">Popular</Link>
-                <Link to="/movies/top_rated">Top Rated</Link>
-                <Link to="/movies/upcoming">Upcoming</Link>
+                <Link to="/"><img className="header__icon" src="flixbase/public/flixbaselogo.png" /></Link>
+                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
+                <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
+                <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
             </div>
         </div>
     )
 }
 
+export default Header
