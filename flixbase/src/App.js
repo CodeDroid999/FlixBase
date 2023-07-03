19f401b900cc4da3d11abce5d990521506fbb0de
CodeDroid99 <johnmwega2002@gmail.com>
2023-07-03 19:17:16 +0300

    add movieList component

--- flixbase/src/App.js
+++ flixbase/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Header from './components/header/Header';
+import Home from './pages/home/home';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
         <Router>
           <Header />
             <Routes>
-                <Route index element={<span>Home Page</span>}></Route>
+                <Route index element={<Home/>}></Route>
                 <Route path="movie/:id" element={<span>Movie Page</span>}></Route>
                 <Route path="movies/:type" element={<span>Movie list Page</span>}></Route>
                 <Route path="/*" element={<span>Error Page</span>}></Route>
