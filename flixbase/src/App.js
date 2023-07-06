10cfaac2ee2c9e3f8a317aea0264faf31be1207a
CodeDroid99 <johnmwega2002@gmail.com>
2023-07-06 21:57:35 +0300

    add project cover image

--- flixbase/src/App.js
+++ flixbase/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/header/Header';
 import Home from './pages/home/home';
 import MovieList from './components/movieList/movieList';
 import Movie from './pages/moviedetail/movie';
+import About from './pages/about/about';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
         <Router>
           <Header />
             <Routes>
-                <Route index element={<Home />}></Route>
+                <Route index element={<About />}></Route>
+                <Route path="home"element={<Home />}></Route>
                 <Route path="movie/:id" element={<Movie />}></Route>
                 <Route path="movies/:type" element={<MovieList />}></Route>
                 <Route path="/*" element={<h1>Error Page</h1>}></Route>
--- flixbase/src/pages/about/about.js
+++ flixbase/src/pages/about/about.js
@@ -1,55 +1,65 @@
-import React, { useEffect, useState } from "react"
-import "./about.css"
+import React, { useEffect, useState } from "react";
+import "./about.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 import { Link } from "react-router-dom";
 import MovieList from "../../components/movieList/movieList";
 
-const Home = () => {
+const About = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
 
-    const [ popularMovies, setPopularMovies ] = useState([])
-
-    useEffect(() => {
-        fetch("https://api.themoviedb.org/3/movie/popular?api_key=c4418d6aefd19fe739aeb5a0bc400aa1&language=en-US")
-        .then(res => res.json())
-        .then(data => setPopularMovies(data.results))
-    }, [])
-
-    return (
-        <>
-            <div className="poster">
-                <Carousel
-                    showThumbs={false}
-                    autoPlay={true}
-                    transitionTime={3}
-                    infiniteLoop={true}
-                    showStatus={false}
-                >
-                    {
-                        popularMovies.map(movie => (
-                            <Link style={{textDecoration:"none",color:"white"}} to={`/movie/${movie.id}`} >
-                                <div className="posterImage">
-                                    <img src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`} />
-                                </div>
-                                <div className="posterImage__overlay">
-                                    <div className="posterImage__title">{movie ? movie.original_title: ""}</div>
-                                    <div className="posterImage__runtime">
-                                        {movie ? movie.release_date : ""}
-                                        <span className="posterImage__rating">
-                                            {movie ? movie.vote_average :""}
-                                            <i className="fas fa-star" />{" "}
-                                        </span>
-                                    </div>
-                                    <div className="posterImage__description">{movie ? movie.overview : ""}</div>
-                                </div>
-                            </Link>
-                        ))
-                    }
-                </Carousel>
-                <MovieList />
-            </div>
-        </>
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=c4418d6aefd19fe739aeb5a0bc400aa1&language=en-US"
     )
-}
+      .then((res) => res.json())
+      .then((data) => setPopularMovies(data.results));
+  }, []);
 
-export default Home
+  return (
+    <>
+      <div className="poster">
+        <Carousel
+          showThumbs={false}
+          autoPlay={true}
+          transitionTime={3}
+          infiniteLoop={true}
+          showStatus={false}
+        >
+          {popularMovies.map((movie) => (
+            <Link
+              style={{ textDecoration: "none", color: "white" }}
+              to={`/movie/${movie.id}`}
+            >
+              <div className="posterImage">
+                <img
+                  alt=""
+                  src={`https://image.tmdb.org/t/p/original${
+                    movie && movie.backdrop_path
+                  }`}
+                />
+              </div>
+              <div className="posterImage__overlay">
+              <div className="posterImage__title">
+                  Flixbase
+                </div>
+                <div className="posterImage__title">
+                  Lights, Popcorn, Action!
+                </div>
+                                <div className="posterImage__description">
+                  Dive into the world of movies with Flixbase, the go-to
+                  platform for all your cinematic needs. Whether you're a film
+                  aficionado, a casual viewer, or a budding filmmaker, Flixbase
+                  is here to bring you an unparalleled movie experience.
+                </div>
+              </div>
+            </Link>
+          ))}
+        </Carousel>
+        <MovieList />
+      </div>
+    </>
+  );
+};
+
+export default About;
