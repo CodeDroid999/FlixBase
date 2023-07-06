aa2edf8c24abff00427b0494d95c7d47c727c4c8
CodeDroid99 <johnmwega2002@gmail.com>
2023-07-06 22:56:08 +0300

    add about cards

--- flixbase/src/pages/about/about.js
+++ flixbase/src/pages/about/about.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/style-prop-object */
 import React, { useEffect, useState } from "react";
 import "./about.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
@@ -40,22 +41,75 @@ const About = () => {
                 />
               </div>
               <div className="posterImage__overlay">
-              <div className="posterImage__title">
-                  Flixbase
-                </div>
+                <div className="posterImage__title">Introducing Flixbase</div>
                 <div className="posterImage__title">
                   Lights, Popcorn, Action!
                 </div>
-                                <div className="posterImage__description">
-                  Dive into the world of movies with Flixbase, the go-to
-                  platform for all your cinematic needs. Whether you're a film
-                  aficionado, a casual viewer, or a budding filmmaker, Flixbase
-                  is here to bring you an unparalleled movie experience.
+                <div className="posterImage__description">
+                  <p>
+                    Dive into the world of movies with Flixbase, the go-to
+                    platform for all your cinematic needs. Whether you're a film
+                    aficionado, a casual viewer, or a budding filmmaker,
+                    Flixbase is here to bring you an unparalleled movie
+                    experience.
+                  </p>
+                </div>
+                <div
+                  class="posterImage__description"
+                  style={{ "margin-top": "1rem" }}
+                >
+                  <span className="movie__homeButton movie__Button">
+                    <Link to="/home" style={{ "text-decoration": "none" }}>
+                      Explore
+                    </Link>
+                  </span>
                 </div>
               </div>
             </Link>
           ))}
         </Carousel>
+        <div className="about__card">
+          <div className="posterImage__title">Timeline_____</div>
+          Flixbase is a Portfolio Project for Holberton School, a place where we
+          have honed our skills as aspiring software engineers. This project has
+          been an integral part of our learning journey, allowing us to apply
+          the knowledge and techniques we've gained throughout our coursework.
+          We are grateful to Holberton School for providing us with the
+          opportunity to work on meaningful projects like Flixbase, empowering
+          us to explore our creativity and showcase our abilities.
+        </div>
+        <div className="about__card">
+          <div className="posterImage__title">Inspiration_____</div>
+          Flixbase is more than just a project for us—it's a passion project
+          born out of our deep love for movies and the desire to create a
+          platform that truly celebrates the magic of cinema. Our journey began
+          as a group of avid movie enthusiasts, driven by the idea of building a
+          comprehensive movie database service that would bring together film
+          lovers from all walks of life. We wanted to create a place where
+          people could immerse themselves in the captivating world of movies,
+          discover new favorites, and connect with like-minded individuals who
+          share the same passion.
+        </div>
+        <div className="about__card">
+          <div className="posterImage__title">The Team_____</div>
+          <p>John Mwega -<Link to="https://github.com/CodeDroid999" style={{'text-decoration':'none'}}> GitHub </Link> </p>
+          <p>Lola Smith - GitHub </p>
+          <p>Jelil Fasasi - GitHub </p>
+        </div>
+        <div className="about__card">
+          <div className="posterImage__title">Source_____</div>
+          To explore the code and contribute to the development of Flixbase,
+          please visit our{" "}
+          <Link to="https://github.com/CodeDroid999/FlixBase">
+            {" "}
+            GitHub repository{" "}
+          </Link>
+          . We encourage collaboration and welcome any feedback, suggestions, or
+          contributions from the community. Thank you for joining us on this
+          cinematic journey. Let's dive into the world of movies together with
+          Flixbase!
+        </div>
+
         <MovieList />
       </div>
     </>
